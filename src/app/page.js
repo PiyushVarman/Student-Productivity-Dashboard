bfe1bf0ea0c44064048a79505c128898d2f47ecd
Piyush Varman <varmanpiyush@gmail.com>
2026-08-29 11:06:35 +0530

    add: music block placeholder

--- src/app/page.js
+++ src/app/page.js
@@ -14,28 +14,39 @@ export default function Home() {
           <TooltipContent>User</TooltipContent>
         </Tooltip>
       </div>
-      <div className=" mt-2 *:shadow-2xl w-[99vw] h-[84vh] items-center *:dark:text-black *:rounded-2xl flex flex-row gap-x-5 *:bg-white *:h-full  *:duration-500">
-        <div className="w-[25vw] p-5">
+      {/* Apps */}
+      <div className="mt-2  w-[99vw] h-[84vh] items-start *:dark:text-black  flex flex-row gap-x-5 *:duration-500">
+        <div className="w-[25vw] shadow-2xl bg-white h-full p-5 rounded-2xl">
           <Tabs>
             <TabsList defaultValue="focus">
               <TabsTrigger value="focus"><Timer/>Focus</TabsTrigger>
               <TabsTrigger value="journal"><NotebookPen/>Journal</TabsTrigger>
             </TabsList>
             <TabsContent value="focus">
-              <div className="flex grow bg-gray-300 shadow-md hover:scale-101 duration-200 animate-out rounded-xl py-[80%] items-center justify-center">
+              <div className="flex grow bg-gray-300 shadow-md hover:scale-101 duration-200 animate-out rounded-xl py-[35vh] items-center justify-center">
                 Focus Timer
               </div>
             </TabsContent>
             <TabsContent value="journal">
-              <div className="flex grow bg-gray-300 shadow-md hover:scale-101 duration-200 animate-out rounded-xl py-[80%] items-center justify-center">
+              <div className="flex grow bg-gray-300 shadow-md hover:scale-101 duration-200 animate-out rounded-xl py-[35vh] items-center justify-center">
                 Journal
               </div>
             </TabsContent>
           </Tabs>
         </div>
-        <div className="flex flex-col items-center justify-center relative">
-          <ScrollArea className="w-[50vw] overflow-auto flex flex-col h-full overflow-none">
+        <div className="rounded-2xl shadow-2xl flex flex-col items-center justify-center relative bg-white h-full">
+          <ScrollArea className="w-[50vw] flex flex-col h-[95vh] overflow-hidden">
             <Bubble className="text-xl mt-5 ml-5 text-gray-900 ">
+              <BubbleContent>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </BubbleContent>
+            </Bubble>
+            <Bubble variant="muted" align="end" className="text-xl mt-10 ml-30 text-right">
+              <BubbleContent>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </BubbleContent>
+            </Bubble>
+            <Bubble className="text-xl mt-10 ml-5 text-gray-900 ">
               <BubbleContent>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </BubbleContent>
@@ -53,23 +64,29 @@ export default function Home() {
           </ScrollArea>
           <textarea className="bg-black/50 w-[95%] relative bottom-5 backdrop-blur-xs shadow-xl py-5 rounded-3xl focus:bg-black duration-200 px-10 text-white"></textarea>
         </div>
-        <div className="w-[25vw] p-5">
-          <Tabs defaultValue="todo">
-            <TabsList>
-              <TabsTrigger value="todo"><CircleCheckBig/>To-Do</TabsTrigger>
-              <TabsTrigger value="docs"><Files/>Documents</TabsTrigger>
-            </TabsList>
-            <TabsContent value="todo">
-              <div>
-                To-Do List.
-              </div>
-            </TabsContent>
-            <TabsContent value="docs">
-              <div>
-                Documents.
-              </div>
-            </TabsContent>
-          </Tabs>
+        <div className="rounded-2xl w-[25vw] flex flex-col ">
+          <div className="bg-white h-[55vh] rounded-2xl shadow-2xl">
+            <Tabs defaultValue="todo" className="rounded-2xl p-5">
+              <TabsList>
+                <TabsTrigger value="todo"><CircleCheckBig/>To-Do</TabsTrigger>
+                <TabsTrigger value="docs"><Files/>Documents</TabsTrigger>
+              </TabsList>
+              <TabsContent value="todo">
+                <ScrollArea>
+                  <div>
+                    To-Do List.
+                  </div>
+                </ScrollArea>
+              </TabsContent>
+              <TabsContent value="docs">
+                <div>
+                  Documents.
+                </div>
+              </TabsContent>
+            </Tabs>
+          </div>
+          <div className="h-[26.5vh] shadow-2xl bg-white rounded-2xl mt-5 ">
+          </div>
         </div>
       </div>
     </div>
